080542be5e2503c93dd149c74db4ed20e03389ff
Lisa <lmkirk@gmail.com>
2019-07-19 14:24:24 -0400

    character info displaying as url's

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import {CardHeader} from "@material-ui/core";
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
 import Divider from '@material-ui/core/Divider';
 
 
@@ -17,12 +12,21 @@ export default function CharacterCard(props) {
   console.log(props);
   return (
     <Card>
-      <CardContent
+      <CardMedia
         name={props.name}
         species={props.species}
         homeworld={props.homeworld}
         films={props.films}
       />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">{props.name}</Typography>
+        <Divider variant="middle" component="p" />
+        <Typography gutterBottom variant="h5" component="h2">{props.species}</Typography>
+        <Typography gutterBottom variant="h5" component="h2">{props.homeworld}</Typography>
+        <Typography gutterBottom variant="h5" component="h2">{props.films}</Typography>
+      </CardContent>
+
+
 
     </Card>
   );
